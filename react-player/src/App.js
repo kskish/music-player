37ea1edefef30fa8410685e1a76b502586731a52
kskish <kskish18@gmail.com>
2021-10-21 13:03:07 -0400

    Initial Styling + Adding State

---
+++ react-player/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+//Import Styles
+import "./styles/app.scss";
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+//Import Util
+import data from "./util";
+
+function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
